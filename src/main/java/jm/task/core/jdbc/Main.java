8bbc9640ef0a5dd538a9741c92ab73391b1aa9b3
runmole <therunmole@gmail.com>
2023-07-05 04:28:55 +0300

    oh

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,34 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        List<User> users = new ArrayList<>();
+        users.add(new User("Dima", "Hmurii", (byte) 24));
+        users.add(new User("Vlad", "Skala", (byte) 25));
+        users.add(new User("Tolya", "Bugor", (byte) 52));
+        users.add(new User("Jesus", "Christ", (byte) 33));
+
+        for (User user : users) {
+            userService.saveUser(user.getName(), user.getLastName(), (byte) user.getAge());
+        }
+        List<User> usersTable = userService.getAllUsers();
+        for (User user : usersTable) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,33 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
     public void createUsersTable() {
-
+        super.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        super.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        super.saveUser(name, lastName, age);
+        System.out.println("User с именем - " + name + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
-
+        super.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return super.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        super.cleanUsersTable();
     }
 }
